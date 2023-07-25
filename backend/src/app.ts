34cde9fce01ff2e2f0760b57ccf0bd5cb1d6feea
Karl Haupt <karl@bbd.co.za>
2023-07-25 18:59:50 +0200

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,30 @@
+
+import express from "express";
+import { SequelizeConnection } from './infrastructure/database/SequelizeConnection';
+import db from "./infrastructure/models";
+import { router as userRoutes } from './app/controller/user';
+
+import { ENV } from "./infra/env";
+
 console.log("Hello, World!");
+
+console.log(ENV.TEST_VAR);
+
+const app = express();
+
+process.on("uncaughtException", (err) => {
+    console.log(`ERROR: ${err}`);
+    console.log("Shutting down due to uncaught exception");
+    process.exit(1);
+});
+
+db.sequelize.sync({ force: true })
+SequelizeConnection.getInstance();
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+app.use("/", userRoutes);
 
 const server = app.listen(8080, () => {
   console.log(`Server started on PORT: 8080 in ${process.env.NODE_ENV}`);
