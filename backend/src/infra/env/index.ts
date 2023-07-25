4fcfeef50d8122306a3eb5ed5ccdc0a69bedd43a
vniej002 <juanr@bbd.co.za>
2023-07-25 15:37:35 +0200

    Updated config. And merged login.

--- backend/src/infra/env/index.ts
+++ backend/src/infra/env/index.ts
@@ -1,7 +1,15 @@
 import { config } from "dotenv";
+import * as mysql from 'mysql2/promise';
 
 config();
 
 export const ENV = {
   PORT: process.env.PORT || 3000,
 };
+
+export const db_config : mysql.ConnectionOptions = {
+    host: process.env.HOST,
+    database: process.env.DB,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+}
