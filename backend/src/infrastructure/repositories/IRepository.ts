a535a512d4cbb08e1224cd207cc7425080144650
Karl Haupt <karl@bbd.co.za>
2023-07-27 15:02:05 +0200

    Update to Repository

--- backend/src/infrastructure/repositories/IRepository.ts
+++ backend/src/infrastructure/repositories/IRepository.ts
@@ -1,6 +1,6 @@
 export interface IRepository<T, ID> {
     create(body: T): Promise<T | null>;
-    readByID(id: ID): Promise<T | null>;
+    readByID(id: ID): Promise<T | undefined>;
     update(id: ID, body: T): Promise<T | null>;
     delete(id: ID): Promise<boolean>;
 }
